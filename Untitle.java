92b590d78ddbecf3598fd0b75a60708f26d2c7b3
Riya Tomar <riyatomar70@outlook.com>
2026-07-13 00:27:53 +0530

    first commit

---
+++ Untitle.java
@@ -0,0 +1,10 @@
+public class Untitle{
+    public static void main(String[] args) {
+        int a = 2, b = 4;
+        int sum = a + b ;
+        int diff = b - a;
+        int mult = a*b;
+        float div = b/a;
+        System.out.println(sum+","+diff+","+mult+","+div);
+    }
+}
