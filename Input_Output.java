f1672beddee189dfd04a2673c1257d1c473ef8ed
Riya Tomar <riyatomar70@outlook.com>
2026-07-14 02:21:37 +0530

    your message

---
+++ Input_Output.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Input_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(word);
+        sc.close();
+    }
+}
